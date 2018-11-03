30b617162206a17288f030436bc29efee3ca4165
gourav-immanent <44663652+gourav-immanent@users.noreply.github.com>
2018-11-03 12:16:45 +0530

    Delete start-service.sh

--- start-service.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ `pgrep apache2 -c` -le "0" ]; then
-/etc/init.d/apache2 stop
-pkill -u www-data  
-/etc/init.d/apache2 start
- echo "restarting....."
-SUBJECT="Apache auto restart"
